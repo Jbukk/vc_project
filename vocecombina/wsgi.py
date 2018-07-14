03041cf5483de4d011053cac403e1d161da77430
Johnny <jhsomiranda@gmail.com>
2018-07-14 20:06:25 -0300

    Ajuste

--- vocecombina/wsgi.py
+++ vocecombina/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vocecombina.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "add.settings")
 
 application = get_wsgi_application()
