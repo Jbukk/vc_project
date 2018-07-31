67e67ab5035135b09b66aeb82ce036da26e46659
Johnny <jhsomiranda@gmail.com>
2018-07-31 17:58:40 -0300

    novos arquivos

--- core/views.py
+++ core/views.py
@@ -1,14 +1,32 @@
 # coding=utf-8
 
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import ContactForm
+from django.core.mail import send_mail
+from django.views.generic import View, TemplateView
 
 
+class IndexView(TemplateView):
 
-def index(request):
-    return render(request, 'index.html')
+
+        template_name = 'index.html'
+
+index = IndexView.as_view()
+
 
 
 def contact(request):
-    return render(request, 'contact.html')
+    success = False
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        form.send_email()
+        success = True
+    context = {
+        'form': form,
+        'success': success
+    }
+    return render(request, 'contact.html', context)
+
 
 
